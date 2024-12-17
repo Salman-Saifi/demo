1171b1fad56a57ff0694e30e9735f4553f18739b
Salmansaifi96 <salmansaifi1304@gmail.com>
2024-12-17 14:35:34 +0530

    Salman js starting 17 dec

---
+++ 36.js
@@ -0,0 +1,13 @@
+// Objects are reference type
+// arrays are good but not sufficient
+// for real world data
+// objects store key value pairs
+// objects don't have index
+
+// how to create Objects
+const person = {name:"Salman",age:40}
+console.log(person);
+
+// how to access data from objects
+
+// how to add key value pairs to objects
